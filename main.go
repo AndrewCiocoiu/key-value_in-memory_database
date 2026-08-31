d2cec9d1d2d54960668f682f676dcc9975780ee1
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-08-31 20:57:13 +0300

    Create in-memory global map, setup mutexes for concurrency safety and create the command functionality

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var mu sync.RWMutex
 
 const PORT = ":9000"
 
-func handleConnect(conn net.Conn, mu sync.RWMutex) {
+func handleConnect(conn net.Conn, mu *sync.RWMutex) {
 	defer conn.Close()
 
 	fmt.Fprintf(conn, "Welcome to Andrew's in-memory DB!\n")
@@ -38,8 +38,28 @@ func handleConnect(conn net.Conn, mu sync.RWMutex) {
 
 		switch command := words[0]; command {
 		case "SET":
+			mu.Lock()
+			db[words[1]] = words[2]
+			fmt.Fprintf(conn, "Value set!\n")
+			mu.Unlock()
 		case "GET":
+			mu.RLock()
+
+			if val, ok := db[words[1]]; ok {
+				fmt.Fprintf(conn, "%s\n", val)
+			} else {
+				fmt.Fprintf(conn, "Key does not exist in DB!\n")
+			}
+
+			mu.RUnlock()
 		case "DEL":
+			mu.Lock()
+			if _, ok := db[words[1]]; ok {
+				delete(db, words[1])
+				fmt.Fprintf(conn, "%s Sucesfully deleted!\n", words[1])
+			} else {
+				fmt.Fprintf(conn, "Key does not exist in DB!\n")
+			}
 		default:
 			fmt.Fprintf(conn, "ERROR: Unknown command.\n")
 		}
@@ -66,7 +86,7 @@ func main() {
 			continue
 		}
 
-		go handleConnect(connection, mu)
+		go handleConnect(connection, &mu)
 
 	}
 }
