62709d5f6b4fc63e351b7d79581db8c5fd82bef9
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-08-31 17:35:14 +0300

    Setup a simple TCP connection

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
+
+const PORT = ":9000"
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	listener, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+	defer listener.Close()
+
+	log.Printf("Server is listening on port: %s\n", PORT[1:])
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("%s\n", err)
+		}
+		connection.Write([]byte("Hello, world!\n"))
+
+		connection.Close()
+	}
 }
